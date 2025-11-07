5176cafdc4bf37f1186e78016f88756c31f45058
xDev <94372300+PSock7@users.noreply.github.com>
2025-11-08 00:07:46 +0100

    Fix static file serving path for uploads

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 app.use('/api/inscriptions', inscriptionRoutes)
